1a1480754144e4fe2284c89fb59e7f0e37a0b025
Patrik <patspek@seznam.cz>
2024-12-17 19:38:28 +0100

    stranka

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+function showsidemenu(){
+    const sidemenu = document.querySelector('.sidemenu')
+    sidemenu.style.display= 'flex'
+}
+function closesidemenu(){
+     const sidemenu = document.querySelector('.sidemenu')
+    sidemenu.style.display= 'none'
+}
+
